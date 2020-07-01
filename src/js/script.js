3027f21d68f18ff083de27e0d11eadabc6f07279
gav313 <gav313@gmail.com>
2020-07-01 16:38:30 +0300

    Part #3 - Tab, review, footer section + adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,44 @@
-$(document).ready(function(){
-    $('.slider__inner').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        infinite: true,
-        speed: 300,
-        adaptiveHeight: false,
-        autoplay: false,
-        autoplaySpeed: 3000,
-        centerMode: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png" alt="left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png" alt="right"></button>'
+  var slider = tns({
+    container: '.slider__inner',
+    items: 1,
+    slideBy: '1',
+    autoplay: false,
+    controls: false,
+    nav: true,
+    navPosition: "bottom",
+    navAsThumbnails: true,
+    touch: true
+  });
+
+  document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+  };
+  document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+  };
+
+
+  (function($) {
+    $(function() {
+    
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
   });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault ();
+        $('.catalog-item__info').eq(i).toggleClass('catalog-item__info_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+})
+(jQuery);
